bfe586479733007006f6f05b75b24a9caf26166d
Danny Huang <huangdong@gmail.com>
2020-09-05 10:54:19 -0400

    增加token文件

--- hello.py
+++ hello.py
@@ -1,6 +1,33 @@
-print("新年快乐！！！")
-print("再来一次好不好？")
-print("这一次我们又来了")
-print("Hello MyFirstBot")
-print("好!好!好!好!好!好!")
-print("让我们一起来吧～")
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import os
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def read_file_as_str(file_path):
+    # 判断路径文件存在
+    if not os.path.isfile(file_path):
+        raise TypeError(file_path + " does not exist")
+
+    # 读取文件内容到all_the_text
+    all_the_text = open(file_path).read()
+    # print type(all_the_text)
+    return all_the_text
+
+TOKEN=read_file_as_str('TOKEN')
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+updater.start_polling()
