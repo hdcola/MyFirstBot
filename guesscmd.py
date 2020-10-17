6f6713fc7b688841590cd65cff3cfdeb319382e5
Danny Huang <huangdong@gmail.com>
2020-10-17 11:44:45 -0400

    加入keyboard的教学内容

--- guesscmd.py
+++ guesscmd.py
@@ -1,4 +1,5 @@
-from telegram.ext import Dispatcher,CommandHandler
+from telegram.ext import Dispatcher,CommandHandler,CallbackQueryHandler
+from telegram import InlineKeyboardMarkup,InlineKeyboardButton
 import random
 
 # rnumber = random.randint(1,99)
@@ -22,8 +23,15 @@ def guess(update, context):
         games[chatid] = { 'rnumber':random.randint(1,99), 'member':{} }
     print(games)
 
+    bigButton = InlineKeyboardButton('大',callback_data='big')
+    smallButton = InlineKeyboardButton('小',callback_data='small')
+    okButton = InlineKeyboardButton('确认',callback_data='ok')
+    nullButton = InlineKeyboardButton('无聊',callback_data='null')
+
+    kb = InlineKeyboardMarkup([[bigButton],[smallButton],[okButton,nullButton]])
+
     if len(context.args) == 0:
-        update.message.reply_text(help())
+        update.message.reply_text(help(),reply_markup=kb)
     else:
         if context.args[0].isdigit():
             number = int(context.args[0])
@@ -33,17 +41,22 @@ def guess(update, context):
             else:
                 games[chatid]['member'][fname] = 1
             if number == games[chatid]['rnumber']:
-                update.message.reply_text("你输入的是 %s 系统随机的是 %s 你猜中了！\n 新的随机数字生成了！"%(number,games[chatid]['rnumber']))
+                update.message.reply_text("你输入的是 %s 系统随机的是 %s 你猜中了！\n 新的随机数字生成了！"%(number,games[chatid]['rnumber']),reply_markup=kb)
                 games[chatid]['rnumber'] = random.randint(1,99)
                 games[chatid]['member']={}
             elif number < games[chatid]['rnumber']:
-                update.message.reply_text("你输入的是 %s 系统随机的是 %s 你猜小了！\n %s"%(number,games[chatid]['rnumber'],games[chatid]['rnumber']))
+                update.message.reply_text("你输入的是 %s 系统随机的是 %s 你猜小了！\n %s"%(number,games[chatid]['rnumber'],games[chatid]['rnumber']),reply_markup=kb)
             elif number > games[chatid]['rnumber']:
-                update.message.reply_text("你输入的是 %s 系统随机的是 %s 你猜大了！\n %s"%(number,games[chatid]['rnumber'],games[chatid]['rnumber']))
+                update.message.reply_text("你输入的是 %s 系统随机的是 %s 你猜大了！\n %s"%(number,games[chatid]['rnumber'],games[chatid]['rnumber']),reply_markup=kb)
         else: 
-            update.message.reply_text("你输入的%s不是数字"%context.args[0])
+            update.message.reply_text("你输入的%s不是数字"%context.args[0],reply_markup=kb)
+
+def buttonCallback(update, context):
+    query = update.callback_query
+    query.answer("%s点了%s"%(update.effective_user.first_name,query.data),show_alert=True)
 
 def add_handler(dp:Dispatcher):
     guess_handler = CommandHandler('guess', guess)
     dp.add_handler(guess_handler)
+    dp.add_handler(CallbackQueryHandler(buttonCallback))
 
