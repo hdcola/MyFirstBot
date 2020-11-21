aef510debbc24595100b4db641d7352326b6053b
Danny Huang <huangdong@gmail.com>
2020-11-21 09:00:12 -0500

    为coins加入读存档的功能，并支持打卡

--- coins.py
+++ coins.py
@@ -2,6 +2,7 @@ import random
 from datetime import datetime,timedelta
 from telegram.ext import Dispatcher,CommandHandler
 from telegram import BotCommand
+import config
 
 # {
 #     chatid: {
@@ -14,39 +15,56 @@ from telegram import BotCommand
 #     }
 # }
 
-coins = {}
+coins = config.CONFIG["coins"]
 
 def check_user(chatid,user):
-    uid = user.id
+    uid = str(user.id)
     first_name = user.first_name
     if not chatid in coins.keys():
         coins[chatid] = {}
     if not uid in coins[chatid].keys():
-        coins[chatid][uid] = {'name':first_name,'coins':0,'count':0,'dailytime':datetime.now()}
+        coins[chatid][uid] = {'name':first_name,'coins':0,'count':0,'dailytime':'2002/02/02 00:00:00'}
+
+def get_dailytime(chatid,uid):
+    return datetime.strptime(coins[chatid][uid]['dailytime'],'%Y/%m/%d %H:%M:%S')
+
+def set_dailytime(chatid,uid,time):
+    coins[chatid][uid]['dailytime']=time.strftime('%Y/%m/%d %H:%M:%S')
+    
+def save():
+    config.CONFIG["coins"] = coins
+    config.save_config()
 
 def show_user(chatid,user):
-    uid = user.id
+    uid = str(user.id)
     check_user(chatid,user)
     #  老房东(10):200
     return f"{coins[chatid][uid]['name']}({coins[chatid][uid]['count']}):{coins[chatid][uid]['coins']}"
 
 def add_coins(chatid,user,c):
     check_user(chatid,user)
-    uid = user.id
+    chatid = str(chatid)
+    uid = str(user.id)
     coins[chatid][uid]['coins'] += c
+    save()
 
 def add_count(chatid,user):
+    chatid = str(chatid)
     check_user(chatid,user)
-    uid = user.id
+    uid = str(user.id)
     coins[chatid][uid]['count'] += 1
+    config.CONFIG["coins"] = coins
+    config.save_config()
 
 def daily(chatid,user):
+    chatid = str(chatid)
     check_user(chatid,user)
-    uid = user.id
-    if datetime.now() > coins[chatid][uid]['dailytime']:
+    uid = str(user.id)
+    if datetime.now() > get_dailytime(chatid,uid):
         c = random.randint(1,100)
         coins[chatid][uid]['coins'] += c
-        coins[chatid][uid]['dailytime'] = datetime.now() + timedelta(minutes=5)
+        set_dailytime(chatid,uid,datetime.now() + timedelta(minutes=5))
+        save()
         return c
     else:
         return 0
