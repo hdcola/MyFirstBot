937916f2dad51b1f98ab12e110da612cef728341
Danny Huang <huangdong@gmail.com>
2021-01-10 11:06:55 -0500

    加入了更巧妙的方式

--- info.py
+++ info.py
@@ -2,6 +2,16 @@ from telegram import Update,Animation, PhotoSize
 from telegram.ext import Dispatcher,CommandHandler,CallbackContext
 from json import dumps,loads
 
+msg_type = {
+    "video":["file_id","file_unique_id","width","height","duration"],
+    "photo":["file_id","file_unique_id","width","height","file_size"],
+    "audio":["file_id","file_unique_id","duration","performer","title","mime_type","file_size"],
+    "animation":["file_id","file_unique_id","width","height","duration"],
+    "sticker":["file_id","file_unique_id","width","height","is_animated"],
+    "videomsg":["file_id","file_unique_id","length","duration"],
+    "voicemsg":["file_id","file_unique_id","duration","mime_type","file_size"]
+}
+
 def listtojson(objs):
     r = ""
     count = 0
@@ -14,30 +24,27 @@ def listtojson(objs):
 def tojson(obj):
     return dumps(eval(str(obj)),indent=2)
 
+def getobjinfo (msgtype,msgobj):
+    rmsg = ""
+    for i in msg_type[msgtype]:
+        # rmsg += str(f'{i} = {msgobj.__dict__[i]},\n\n')
+        rmsg += str(f'{i} = {getattr(msgobj,i)},\n\n')
+    return rmsg
+
 def info(update:Update,context: CallbackContext):
     if update.effective_message.reply_to_message:
         msg = update.effective_message.reply_to_message
-        msg_attr = {}
+        rmsg = ""
         if msg.audio:
-            msg_attr["Audio"] = tojson(msg.audio)
+            audio = update.message.reply_to_message.audio
+            rmsg = getobjinfo('audio',audio)
+            update.message.reply_audio(audio,caption=rmsg)
         if msg.animation:
-            msg_attr["Animation"] = tojson(msg.animation)
-        if msg.photo:
-            msg_attr["Photo"] = listtojson(msg.photo)
-        if msg.video:
-            msg_attr["Video"] = tojson(msg.video)
-        if len(msg_attr)==0:
-            msg_attr["不知道是什么东东"] = tojson(msg)
-        r = ""
-        for a in msg_attr:
-            r = f"类型：{a}\n{msg_attr[a]}\n"
-        update.message.reply_text(text=r)
+            animation = update.message.reply_to_message.animation
+            rmsg = getobjinfo('animation',animation)
+            update.message.reply_animation(animation,caption=rmsg)
     else:
         update.message.reply_text(text=tojson(update.effective_message))
 
-def hdinfo(update:Update,context: CallbackContext):
-    update.message.reply_text(text=tojson(update.effective_message))
-
 def add_dispatcher(dp: Dispatcher):
     dp.add_handler(CommandHandler(["info"], info))
-    dp.add_handler(CommandHandler(["hdinfo"], hdinfo))
